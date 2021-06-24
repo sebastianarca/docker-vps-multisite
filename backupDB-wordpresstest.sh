45ce21fd87c3288be20d8156f1136349e1a92f2d
sebastianarca <sebastian@navebinario.com>
2021-06-23 22:22:19 -0300

    Mejoras en estructura para wordpress ejemplo

--- backupDB-wordpresstest.sh
+++ backupDB-wordpresstest.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 _now=$(date +"%Y%m%d")
 _mysqlContainer='wp_wordpresstest_mysql'
-_mysqlPass='1234'
-_mysqlUser='root'
+_mysqlUser='wp_mysql_user'
+_mysqlPass='wp_mysql_pass_1234'
 _mysqlDBName='custom'
 _directorioBackup='/tmp/sql_backup'
 
